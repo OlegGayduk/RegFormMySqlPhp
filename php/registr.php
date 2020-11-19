b97539a9acc913b237c5879ca3e8935898009ab0
Oleg <olezhka-markov-2000@mail.ru>
2020-11-19 20:08:38 +0300

    make

--- php/registr.php
+++ php/registr.php
@@ -17,11 +17,11 @@ if(isset($_COOKIE['id']) && isset($_COOKIE['pass'])) header('Location: php/welco
         <form class="main-form" method="POST" action="registration.php"> 
             <p><label class="login-text">Ваш Email: </label>
             <input class="login-field" name="login" type="email" size="40" maxlength="40" /></p> 
-            <p><label class="pass-text">Придумайте пароль: </label>
-            <input class="pass-field" name="pass" type="password" size="40" maxlength="40" /></p> 
+            <p><label class="registr-pass-text">Придумайте пароль: </label>
+            <input class="registr-pass-field" name="pass" type="password" size="40" maxlength="40" /></p> 
             <p><label class="alias-text">Придумайте никнейм: </label>
             <input class="alias-field" name="alias" type="text" size="12" maxlength="12" /></p> 
-            <p><input class="registr-btn" type="submit" value="Зарегистрироваться"></p> 
+            <p><input class="registr-btn" type="submit" value="Зарегистрироваться" onclick="client.registr()"></p> 
             <p><a class="index" href='../index.php'>На главную</a></p>
         </form>
 	</div>
